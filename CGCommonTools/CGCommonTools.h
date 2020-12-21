9f387ea228b8c1cd1313fb1900a99d22e2c2186c
Chenhde <669775990@qq.com>
2020-12-21 11:17:20 +0800

    添加头文件

---
+++ CGCommonTools/CGCommonTools.h
@@ -0,0 +1,22 @@
+//
+//  CGCommonTools.h
+//  TestIMList
+//
+//  Created by 陈鸿德 on 2020/12/21.
+//
+
+#ifndef CGCommonTools_h
+#define CGCommonTools_h
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+
+#import "Common.h"
+#import "BaseModel.h"
+#import "UIButton+Tools.h"
+
+
+#endif /* CGCommonTools_h */
+
+
+
+
